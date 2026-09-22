e15eb906e786148a353c02d0f24bb72d920e807a
hashiryo <hashimotoryoma@gmail.com>
2026-09-22 14:44:26 +0900

    inline 化を公平にしたり、一時変数の置き方変えたり修正

--- problems/gf2-64-mul/submissions/pclmul_pclmul.hpp
+++ problems/gf2-64-mul/submissions/pclmul_pclmul.hpp
@@ -4,13 +4,13 @@
 namespace gf2_64_mul_pclmul_baseline {
 PCLMUL inline u64 mul(u64 a, u64 b) {
  __m128i v= _mm_clmulepi64_si128(_mm_cvtsi64_si128(a), _mm_cvtsi64_si128(b), 0);
- b = (u64)_mm_clmulepi64_si128(v, _mm_cvtsi64_si128(0b11011), 1)[0];
- a = (u64)v[0] ^ ((u8[]){0, 27, 45, 54, 90, 65, 119, 108})[((u64)v[1]) >> 60];
- return a^b;
+ b= (u64)_mm_clmulepi64_si128(v, _mm_cvtsi64_si128(0b11011), 1)[0];
+ a= (u64)v[0] ^ ((u8[]){0, 27, 45, 54, 90, 65, 119, 108})[((u64)v[1]) >> 60];
+ return a ^ b;
 }
 }
 struct GF2_64Op {
- PCLMUL static vector<u64> run(const vector<u64>& as, const vector<u64>& bs) {
+ static vector<u64> run(const vector<u64>& as, const vector<u64>& bs) {
   using gf2_64_mul_pclmul_baseline::mul;
   vector<u64> ans(as.size());
   for(size_t i= 0; i < as.size(); ++i) ans[i]= mul(as[i], bs[i]);
