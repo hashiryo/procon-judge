eb9b25273f80bf9dc7a7aa9aa9b66e6df296b576
hashiryo <hashimotoryoma@gmail.com>
2026-09-23 16:12:16 +0900

    shared の mul2 をより効率的なものに差し替え

--- problems/_shared/gf2-64/mul2.hpp
+++ problems/_shared/gf2-64/mul2.hpp
@@ -14,10 +14,9 @@ namespace gf2_64_pclmul {
 const __m256i RED_TABLE= _mm256_setr_epi8(0, 27, 45, 54, 90, 65, 119, 108, 0, 0, 0, 0, 0, 0, 0, 0, 0, 27, 45, 54, 90, 65, 119, 108, 0, 0, 0, 0, 0, 0, 0, 0);
 template <int IMM= 0> GNU_TARGET("vpclmulqdq") inline __m256i mul2(const __m256i& a_vec, const __m256i& b_vec) {
  __m256i prod= _mm256_clmulepi64_epi128(a_vec, b_vec, IMM);
- __m256i d_full= _mm256_xor_si256(prod, _mm256_slli_epi64(prod, 1));
- __m256i red1_shift= _mm256_srli_si256(_mm256_xor_si256(d_full, _mm256_slli_epi64(d_full, 3)), 8);
- __m256i indices= _mm256_srli_si256(_mm256_srli_epi64(prod, 60), 8);
- return _mm256_xor_si256(_mm256_xor_si256(prod, red1_shift), _mm256_shuffle_epi8(RED_TABLE, indices));
+ __m256i h= _mm256_srli_si256(prod, 8);
+ __m256i d= _mm256_xor_si256(h, _mm256_slli_epi64(h, 1));
+ return _mm256_xor_si256(_mm256_xor_si256(prod, _mm256_shuffle_epi8(RED_TABLE, _mm256_srli_epi64(h, 60))), _mm256_xor_si256(d, _mm256_slli_epi64(d, 3)));
 }
 GNU_TARGET("vpclmulqdq") inline __m256i mul2h(const __m256i& a_vec, const __m256i& b_vec) { return mul2<0x11>(a_vec, b_vec); }
 inline pair<u64, u64> unpack(const __m256i& vec) { return make_pair(u64(_mm256_extract_epi64(vec, 0)), u64(_mm256_extract_epi64(vec, 2))); }
