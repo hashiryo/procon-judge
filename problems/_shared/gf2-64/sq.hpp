940963a03e43179d876235df509bd94e0ae34dfb
hashiryo <hashimotoryoma@gmail.com>
2026-09-24 21:59:09 +0900

    shared を更新

--- problems/_shared/gf2-64/sq.hpp
+++ problems/_shared/gf2-64/sq.hpp
@@ -7,22 +7,17 @@
 // それ以外の problem (div/pow/sqrt/log) は building block として使用 OK。
 #include "_common.hpp"
 namespace gf2_64_pclmul {
-inline u64 spread_bits(u32 a) {
-#ifdef __BMI2__
- return _pdep_u64(u64(a), 0x5555555555555555ull);
-#else
- // fallback: bit interleave for 32-bit input
- u64 x= a;
- x= (x | (x << 16)) & 0x0000FFFF0000FFFFull;
- x= (x | (x << 8)) & 0x00FF00FF00FF00FFull;
- x= (x | (x << 4)) & 0x0F0F0F0F0F0F0F0Full;
- x= (x | (x << 2)) & 0x3333333333333333ull;
- x= (x | (x << 1)) & 0x5555555555555555ull;
- return x;
-#endif
-}
+const __m128i mask_lo= _mm_set1_epi8(0x0F);
+const __m128i spread_tbl= _mm_setr_epi8(0x00, 0x01, 0x04, 0x05, 0x10, 0x11, 0x14, 0x15, 0x40, 0x41, 0x44, 0x45, 0x50, 0x51, 0x54, 0x55);
+constexpr u8 RED[4]= {0, 27, 90, 65};
 inline u64 sq(u64 a) {
- u64 h= spread_bits(u32(a >> 32)), d= h ^ (h << 1);
- return spread_bits(u32(a)) ^ ((u8[]){0, 27, 90, 65})[a >> 62] ^ d ^ (d << 3);
+ // spread_tbl[v] = bit i ∈ v を bit 2i に展開した 8-bit 値
+ //   v=0x0:0x00, 0x1:0x01, 0x2:0x04, 0x3:0x05, 0x4:0x10, ... , 0xF:0x55
+ __m128i v= _mm_set_epi64x(0, a);
+ __m128i nib= _mm_and_si128(_mm_unpacklo_epi8(v, _mm_srli_epi16(v, 4)), mask_lo);
+ __m128i squared= _mm_shuffle_epi8(spread_tbl, nib);
+ // squared の下位 64 bit が a の下位 32 bit の平方、上位 64 bit が a の上位 32 bit の平方
+ u64 h= squared[1], d= h ^ (h << 1);
+ return squared[0] ^ RED[a >> 62] ^ d ^ (d << 3);
 }
 }
