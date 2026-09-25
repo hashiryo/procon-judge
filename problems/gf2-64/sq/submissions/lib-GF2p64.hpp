e62f544dc5c21d72531c1ce239a4a4a31101f5d0
hashiryo <hashimotoryoma@gmail.com>
2026-09-25 22:16:12 +0900

    GF2p64を作り始め, mul,sq,sqrtだけとりあえず提出

---
+++ problems/gf2-64/sq/submissions/lib-GF2p64.hpp
@@ -0,0 +1,11 @@
+#include "mylib/algebra/GF2p64.hpp"
+#include "vector"
+using namespace std;
+struct GF2_64Op {
+ using u64= unsigned long long;
+ static vector<u64> run(const vector<u64>& as) {
+  vector<u64> ans(as.size());
+  for(size_t i= 0; i < as.size(); ++i) ans[i]= (u64)GF2p64(as[i]).square();
+  return ans;
+ }
+};
