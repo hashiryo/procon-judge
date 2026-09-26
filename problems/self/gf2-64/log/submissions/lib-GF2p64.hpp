1c5183903a7702bfdc40b7f77157a7d902b13088
hashiryo <hashimotoryoma@gmail.com>
2026-09-26 22:20:25 +0900

    余計なの消す

--- problems/self/gf2-64/log/submissions/lib-GF2p64.hpp
+++ problems/self/gf2-64/log/submissions/lib-GF2p64.hpp
@@ -3,7 +3,6 @@
 using namespace std;
 using u64= unsigned long long;
 inline vector<u64> run(const vector<u64>& as) {
- using namespace gf2p64_internal;
  vector<u64> ans(as.size());
  for(size_t i= 0; i < as.size(); ++i) ans[i]= GF2p64(as[i]).log();
  return ans;
